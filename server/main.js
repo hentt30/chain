e1218001f8e731e50d591bad57091e109c2c3a5b
rubens <rubens.miguek@gmail.com>
2019-04-06 16:56:22 -0300

    rebooting

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
-// Server entry point, imports all server code
+import { Meteor } from 'meteor/meteor';
 
-import '/imports/startup/server';
-import '/imports/startup/both';
+Meteor.startup(() => {
+  // code to run on server at startup
+});
