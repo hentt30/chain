aa20fde3c2c4c6d87c22d34554c787ca21a86891
rubens <rubens.miguek@gmail.com>
2019-04-25 20:57:17 -0300

    [x] authentication system

--- imports/routes.jsx
+++ imports/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
+import { Meteor } from 'meteor/meteor';
 
 // route components
 import App from './ui/App.jsx';
@@ -8,6 +9,7 @@ import SignUp from './ui/SignUp.jsx';
 import SignUpSubjects from './ui/SignUpSubjects.jsx';
 import Chat from './ui/Chat.jsx';
 import PageNotFound from './ui/PageNotFound.jsx';
+import PrivateRoute from './ui/PrivateRoute.jsx';
 
 
 const browserHistory = createBrowserHistory();
@@ -17,8 +19,8 @@ export const renderRoutes = () => (
         <Switch>
             <Route exact path="/" component={App}/>
             <Route exact path="/signup" component={SignUp}/>
-            <Route exact path="/signup-subjects" component={SignUpSubjects}/>
-            <Route exact path="/chat" component={Chat}/>
+            <PrivateRoute exact path="/signup-subjects" component={SignUpSubjects}/>
+            <PrivateRoute exact path="/chat" component={Chat}/>
             <Route component={PageNotFound}/>
         </Switch>
     </Router>
---
+++ imports/ui/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Meteor } from 'meteor/meteor';
+import { Redirect, Route } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+    // Add your own authentication on the below line.
+    const isLoggedIn = Meteor.user();
+
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                isLoggedIn ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+                )
+            }
+        />
+    )
+};
+
+export default PrivateRoute
