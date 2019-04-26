7b4dc529b7d960d36024da53128b0977a5bfcbef
rubens <rubens.miguek@gmail.com>
2019-04-26 01:00:52 -0300

    [x] Fix Private Route

--- imports/ui/PrivateRoute.jsx
+++ imports/ui/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Redirect, Route } from 'react-router-dom'
 const PrivateRoute = ({ component: Component, ...rest }) => {
 
     // Add your own authentication on the below line.
-    const isLoggedIn = !Meteor.user();
+    const isLoggedIn = Meteor.user();
 
     return (
         <Route
