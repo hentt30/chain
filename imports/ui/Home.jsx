fc912d9fd6f142ab6a0d657dd4b9027bd1311427
rubens <rubens.miguek@gmail.com>
2019-05-01 14:52:15 -0300

    fix home

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -20,57 +20,7 @@ const SubTitle = styled.h1`
     margin-bottom: 50px;
 `;
 
-const WrapperSpanInput = styled.div`
-
-    position: relative;
-    margin: 10px;
-`;
-
-const EmailHolder = styled.input`
-    
-    color: #0360ad;
-    width: 200%;
-    padding:2px;
-    border: 0px;
-    border-bottom: 1px solid #0360ad;
-    &:focus{
-        outline:none;
-    }
-`;
-
-const TitleInInput = styled.span`
-
-    position: absolute;
-    left: 2px;
-    transition: 0.8s ease;
-    color: ${props => (props.isFocused ? "#0360ad" : "#a2a6ad")};
-    top: ${props => (props.isFocused ? "-15px" : "auto")};
-    font-size: ${props => (props.isFocused ? "0.75em" : "auto")};
-`;
-
-const Image = styled.img`
-  
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height:auto;
-    
-`;
-
-const PasswordHolder = styled.input`
-    padding:2px;
-    width: 200%;
-    border: 0px;
-    border-bottom: 1px solid #0360ad;
-    color:#0360ad;
-
-    &:focus{
-        outline:none;
-    }
-`;
-
-const SubmitButton = styled.button`
+const EnterButton = styled.button`
 
     color: white;
     width: 200px;
@@ -104,85 +54,15 @@ const CenterWrapper = styled.div`
 
 `;
 
-const StayAway1 = styled.div`
-
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-const StayAway2 = styled.div`
-
-    width:200%;
-    margin-bottom: 50px;
-`;
-
-const PutInSameLineWrapper = styled.div`
-
-    display: flex;
-    flex-direction: row;
-`;
-
-const LittleText = styled.a`
-
-    font-size: 10px;
-    text-algin:center;
-    color:#0360ad;
-`;
-
 export default class Home extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            emailIsFocused: false,
-            passwordIsFocused: false,
-        };
-
-        this.handleOnFocusEmail = this.handleOnFocusEmail.bind(this);
-        this.handleOnFocusPassword = this.handleOnFocusPassword.bind(this);
-    }
-
-
-    /*FUNCTIONS*/
-
-    handleChangeEmail = event => {
-        this.setState({ email: event.target.value });
-    };
-
-    handleChangePassword = event => {
-        this.setState({ password: event.target.value });
-    };
-
-    handleOnFocusEmail() {
-        this.setState({ emailIsFocused: true, });
-    }
-
-    handleOnFocusPassword() {
-        this.setState({ passwordIsFocused: true, });
-    }
-
-    login = () => {
-        Meteor.loginWithPassword(this.state.email, this.state.password);
-        console.log('logged!');
-    };
 
     render() {
         return (
-            
             <CenterWrapper>
-               
                 <Title>Chain</Title>
                 <SubTitle>Connecting People Through Ideas</SubTitle>
-                <Link to="/signup"><SubmitButton onClick={this.login}> <b>Sign Up</b> </SubmitButton></Link>
-                <Link to="/login"><SubmitButton onClick={this.login}> <b>Login</b></SubmitButton></Link>
-                
-                
-               
+                <Link to="/signup"><EnterButton> <b>Sign Up</b> </EnterButton></Link>
+                <Link to="/login"><EnterButton> <b>Login</b></EnterButton></Link>
             </CenterWrapper>
         );
     }
